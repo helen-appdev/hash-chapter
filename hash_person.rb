368f84c7fd448c08c4081664194a0cd80eb70abf
helenavocado <hwang92@uchicago.edu>
2021-10-16 13:16:11 +0000

    asfds

--- hash_person.rb
+++ hash_person.rb
@@ -1,2 +1,7 @@
 # Ask for a name, age, and occupation; store the values in a Hash; display the Hash.
+
 p "Enter a name, age, and occupation separated by spaces:"
+
+info = gets.chomp.split
+info2 = {:name => info[0], :age => info[1].to_i, :occupation => info[2]}
+p info2
